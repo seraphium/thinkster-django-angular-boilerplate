adfaebc9a8f8dafa8a1262edc1ce177b52bb7a01
Jackie Zhang <viperking@163.com>
2017-04-16 15:39:58 +0800

    finished tutorial

--- static/javascripts/thinkster.routes.js
+++ static/javascripts/thinkster.routes.js
@@ -22,8 +22,21 @@
       controllerAs: 'vm',
       templateUrl: '/static/templates/authentication/login.html'
     })
+    .when('/', {
+      controller: 'IndexController',
+      controllerAs: 'vm',
+      templateUrl: '/static/templates/layout/index.html'
+    })
+    .when('/+:username', {
+        controller: 'ProfileController',
+        controllerAs: 'vm',
+        templateUrl: '/static/templates/profiles/profile.html'
+    })
+    .when('/+:username/settings', {
+      controller: 'ProfileSettingsController',
+      controllerAs: 'vm',
+      templateUrl: '/static/templates/profiles/settings.html'
+    })
     .otherwise('/');
-
     }
-
 })();
